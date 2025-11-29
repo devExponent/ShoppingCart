8bf99421c97e9f2e69b4c51f265e5d9ac22cce38
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-11-29 17:55:52 +0200

    cartupdated

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import Product from "./components/Product.jsx";
-import CartContextProvider from "./store/CartContext.jsx";
 
 function App() {
   return (
     <>
-      <CartContextProvider>
-        <Header />
-        <Shop>
-          {DUMMY_PRODUCTS.map((product) => (
-            <li key={product.id}>
-              <Product {...product} />
-            </li>
-          ))}
-        </Shop>
-      </CartContextProvider>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
